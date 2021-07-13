544f260de2b482e8f65c436087838000cb73f1a1
pchan74 <60635582+pchan74@users.noreply.github.com>
2021-07-13 14:07:11 +0900

    second

--- models/User.js
+++ models/User.js
@@ -1,1 +1,36 @@
-const mongoose = require('mongooo  ')
+const mongoose = require('mongoose')
+
+const usrSchema = mongoose.Schema({
+    name: {
+        type: String,
+        maxLength: 50
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: 1
+    },
+    password: {
+        type: String,
+        maxLength: 50
+    },
+    lastname: {
+        type: String,
+        maxLength: 50
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    image: String,
+    token: {
+        type: String
+    },
+    tikenExp: {
+        type: Number
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = {User}
